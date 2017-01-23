bb89ef6ef13c79cd132cac8b23ea499562eca260
Niklas Sletteland <niklassletteland@gmail.com>
2017-01-22 19:42:14 -0800

    add run() for local testing; center text

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
+# -*- coding: utf8 -*-
+
 from flask import Flask
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-# Note: We don't need to call run() since our application is embedded within
-# the App Engine WSGI application server.
-
 
 @app.route('/')
-def hello():
-    return '__________ CØIN __________'
+def main():
+    return '<div style="display:flex; justify-content:center; align-items:center; height:100%">__________ CØIN __________</div>'
 
 
 @app.errorhandler(404)
 def page_not_found(e):
     return 'sorry, nothing to see here ¯\_(ツ)_/¯', 404
+
+# Note: We don't need to call run() since our application is embedded within
+# the App Engine WSGI application server.
+if __name__ == "__main__":
+    app.run()
+    
